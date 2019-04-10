26e1ce4db22e80d371630e426f2529b3739bb974
szymanskir <rafal.szymanski@ttpsc.pl>
2019-04-10 15:22:52 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        //1.1
+        System.out.println("Zadanie 1.1");
+        boolean b = true;
+        int i = 1;
+        int i2 = 2;
+        char c = 'c';
+        long l = 2;
+        float f = 1.1f;
+        double d = 2.22;
+        String s = "wyraz";
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(s);
+        //1.2
+        System.out.println("Zadanie 1.2");
+        System.out.println(add(i,i2));
+        //1.3
+        System.out.println("Zadanie 1.3");
+        
+    }
+}
